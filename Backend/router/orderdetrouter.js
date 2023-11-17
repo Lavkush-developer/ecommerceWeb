cfb5da3637c158e954274cfe82f9b39cd3c937a3
Lavkush-developer <lavkushm4545@gmail.com>
2023-11-17 17:30:28 +0530

    Completed Website

---
+++ Backend/router/orderdetrouter.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const orderdetrouter=express.Router();
+const Orderdetail=require('../models/Orderdetail');
+
+orderdetrouter.get("/:id",async(req,res)=>{
+    const r=await Orderdetail.find({orderno:req.params.id});
+    res.json(r);
+  })
+
+  module.exports=orderdetrouter
